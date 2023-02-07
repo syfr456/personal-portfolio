ab15751c17a576e140d8933965e294c31410ebbe
syfr456 <sekarsyafira225@gmail.com>
2023-02-07 20:52:08 +0700

    home-contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '',
+    component: HomeComponent
+  },
+  { path: 'about',
+    component: AboutComponent
+  },
+  { path: 'contact',
+    component: ContactComponent
+  },
+  { path: '**',
+    redirectTo: ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
